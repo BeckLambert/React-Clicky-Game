bd1ebb16e0816da7103046319f30f64c471a5cbf
lambertco <blambertco@gmail.com>
2019-11-04 19:44:48 -0700

    working on main logic, still throwing errors for card and header

--- clickygame-app/src/App.js
+++ clickygame-app/src/App.js
@@ -7,21 +7,57 @@ import "./App.css";
 
 class App extends Component {
   state = {
-    friends
+    friends,
+    score: 0,
+    highscore: 0
   };
 
+  shuffleDeck = (array) => {
+    const shuffledDeck = array.sort(function (a, b) { return 0.5 - Math.random() });
+    this.setState({ friends: shuffledDeck })
+  }
+
+  gameOver = () => {
+    if (this.state.score > this.state.highscore) {
+      this.setState({highscore: this.state.score}, function() {
+        console.log(this.state.highscore);
+      });
+    }
+    this.state.friends.forEach(card => {
+      friends.count = 0;
+    });
+    alert(`Game Over :( \nscore: ${this.state.score}`);
+    this.setState({score: 0});
+    return true;
+  }
+
+  clickCount = id => {
+    this.state.friends.find((o, i) => {
+      if (o.id === id) {
+        if(friends[i].count === 0){
+          friends[i].count = friends[i].count + 1;
+          this.setState({score : this.state.score + 1}, function(){
+            console.log(this.state.score);
+          });
+          this.state.friends.sort(() => Math.random() - 0.5)
+          return true; 
+        } else {
+          this.gameOver();
+        }
+      }
+    });
+  }
+ 
   render() {
     return (
       <Wrapper>
-        <Title>The Cluster</Title>
-        {this.state.friends.map(friend => (
-          <FriendCard
-            id={friend.id}
-            key={friend.id}
-            name={friend.name}
-            image={friend.image}
-            occupation={friend.occupation}
-            location={friend.location}
+        <Header score={this.state.score} highscore={this.state.highscore}>Clicky Game</Header>
+        {this.state.cards.map(card => (
+          <Card
+            clickCount={this.clickCount}
+            id={friends.id}
+            key={friends.id}
+            image={friends.image}
           />
         ))}
       </Wrapper>
